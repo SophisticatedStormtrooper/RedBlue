dbc0360b89c1cfbbd5d22286143eb4e89d753842
ζέυς <Zeus@michaels-mbp.attlocal.net>
2015-11-30 21:40:02 -0800

    added new files

--- RedBlue/ViewController.swift
+++ RedBlue/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoBlue: UIImageView!
+    @IBOutlet weak var logoRed: UIImageView!
+    @IBOutlet weak var hideBlue: UIButton!
+    @IBOutlet weak var hideRed: UIButton!
+    @IBOutlet weak var resetButt: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func resetdefalt(sender: AnyObject) {
+        logoRed.hidden = false
+        logoBlue.hidden = false
+        hideBlue.hidden = false
+        hideRed.hidden = false
+        resetButt.hidden = false
+    }
+    
+    @IBAction func HideTheBlue(sender: AnyObject) {
+        
+        logoBlue.hidden = true
+        
+    
+    }
+    
+    @IBAction func HideTheRed(sender: AnyObject) {
+   
+        logoRed.hidden = true
+        
+    }
 }
 
